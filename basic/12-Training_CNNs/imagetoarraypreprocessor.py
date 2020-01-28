70f7c7806b631312f45ccd0e99d412c798105ca9
j-bd <recodata.out@gmail.com>
2020-01-28 16:49:23 +0100

    image preprocess method

--- basic/12-Training_CNNs/imagetoarraypreprocessor.py
+++ basic/12-Training_CNNs/imagetoarraypreprocessor.py
@@ -12,6 +12,14 @@ from keras.preprocessing.image import img_to_array
 class ImageToArrayPreprocessor:
     '''We wrap img_to_array function inside a new classwith a special preprocess
     function'''
-    def __init__(self, dataFormat=None):
-        # store the image data format
-        self.dataFormat = dataFormat
+    def __init__(self, data_format=None):
+        '''Initialize elements for preprocessing'''
+        # store the image data format. This value defaults to None, which
+        # indicates that the setting inside keras.json should be used
+        self.data_format = data_format
+
+    def preprocess(self, image):
+        '''Apply the Keras utility function that correctly rearranges the
+        dimensions of the image. Returns a new NumPy array with the channels
+        properly ordered'''
+        return img_to_array(image, data_format=self.data_format)
