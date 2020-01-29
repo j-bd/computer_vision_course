0ea41da9ffc3228334f64e5fcef6f68e36cc8b48
j-bd <recodata.out@gmail.com>
2020-01-29 16:55:00 +0100

    parser

--- basic/12-Training_CNNs/shallownet_vegetable.py
+++ basic/12-Training_CNNs/shallownet_vegetable.py
@@ -20,3 +20,31 @@ from imagetoarraypreprocessor import ImageToArrayPreprocessor
 from data_tools import SimplePreprocessor
 from data_tools import SimpleDatasetLoader
 from shallownet import ShallowNet
+
+
+def arguments_parser():
+    '''Retrieve user data command'''
+    parser = argparse.ArgumentParser(
+        prog="Vegetable Classification",
+        usage='''%(prog)s [Application of SwallowNet CNNs structure]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch custom training execution:
+        -------------------------------------
+        python3 shallownet_vegetable.py --dataset path/to/folder/containing_image
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-d", "--dataset", required=True, help="path to input dataset"
+    )
+    args = vars(parser.parse_args())
+    return args
+
+def main():
+    '''Launch main process'''
+    args = arguments_parser()
+
+
+if __name__ == "__main__":
+    main()
