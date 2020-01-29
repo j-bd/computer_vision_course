5b1e5f70a15a8ad462aa14e78f6686df45f0dbbd
j-bd <recodata.out@gmail.com>
2020-01-29 17:11:55 +0100

    preprocessing function

--- basic/12-Training_CNNs/shallownet_vegetable.py
+++ basic/12-Training_CNNs/shallownet_vegetable.py
@@ -7,6 +7,7 @@ Created on Wed Jan 29 16:37:49 2020
 """
 
 import argparse
+import logging
 
 from imutils import paths
 import matplotlib.pyplot as plt
@@ -21,12 +22,14 @@ from data_tools import SimplePreprocessor
 from data_tools import SimpleDatasetLoader
 from shallownet import ShallowNet
 
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
 
 def arguments_parser():
     '''Retrieve user data command'''
     parser = argparse.ArgumentParser(
         prog="Vegetable Classification",
-        usage='''%(prog)s [Application of SwallowNet CNNs structure]''',
+        usage='''%(prog)s [Application of ShallowNet CNNs structure]''',
         formatter_class=argparse.RawDescriptionHelpFormatter, description='''
         To lauch custom training execution:
         -------------------------------------
@@ -41,9 +44,28 @@ def arguments_parser():
     args = vars(parser.parse_args())
     return args
 
+def preprocessing(args):
+    '''Prepare data and labels'''
+    # grab the list of images that we’ll be describing
+    logging.info(" Loading images...")
+    image_paths = list(paths.list_images(args["dataset"]))
+
+    # initialize the image preprocessors
+    resize = SimplePreprocessor(32, 32)
+    image_to_array = ImageToArrayPreprocessor()
+
+    # load the dataset from disk then scale the raw pixel intensities
+    # to the range [0, 1]
+    dataset_loader = SimpleDatasetLoader(preprocessors=[resize, image_to_array])
+    (data, labels) = dataset_loader.load(image_paths, verbose=50)
+    data = data.astype("float") / 255.0
+    return data, labels
+
 def main():
     '''Launch main process'''
     args = arguments_parser()
+    data, labels = preprocessing(args)
+
 
 
 if __name__ == "__main__":
