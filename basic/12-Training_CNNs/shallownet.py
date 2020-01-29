3278caba064e6c426098070e35a57b781ed6efaa
j-bd <recodata.out@gmail.com>
2020-01-29 18:59:48 +0100

    model summary

--- basic/12-Training_CNNs/shallownet.py
+++ basic/12-Training_CNNs/shallownet.py
@@ -36,6 +36,6 @@ class ShallowNet:
         model.add(Flatten())
         model.add(Dense(classes))
         model.add(Activation("softmax"))
-
+        model.summary()
         # return the constructed network architecture
         return model
