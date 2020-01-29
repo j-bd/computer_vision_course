0a3f84f971f31b8d3a96d1622cbf10f5d800639c
j-bd <recodata.out@gmail.com>
2020-01-29 18:05:28 +0100

    model construction and evaluation

--- basic/12-Training_CNNs/shallownet_vegetable.py
+++ basic/12-Training_CNNs/shallownet_vegetable.py
@@ -45,7 +45,7 @@ def arguments_parser():
     return args
 
 def preprocessing(args):
-    '''Prepare data and labels'''
+    '''Prepare data and labels under numpy format'''
     # grab the list of images that we’ll be describing
     logging.info(" Loading images...")
     image_paths = list(paths.list_images(args["dataset"]))
@@ -66,6 +66,41 @@ def main():
     args = arguments_parser()
     data, labels = preprocessing(args)
 
+    # partition the data into training and testing splits using 75% of the data
+    # for training and the remaining 25% for testing
+    logging.info(" Splitting data...")
+    (train_x, test_x, train_y, test_y) = train_test_split(
+        data, labels, test_size=0.25, random_state=42
+    )
+    # convert the labels from integers to vectors
+    train_y = LabelBinarizer().fit_transform(train_y)
+    test_y = LabelBinarizer().fit_transform(test_y)
+
+    # initialize the optimizer and model
+    logging.info(" Compiling model...")
+    opt = SGD(lr=0.005)
+    model = ShallowNet.build(width=32, height=32, depth=3, classes=3)
+    model.compile(
+        loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"]
+    )
+
+    # train the network
+    logging.info(" Training network...")
+    history = model.fit(
+        train_x, train_y, validation_data=(test_x, test_y), batch_size=32,
+        epochs=100, verbose=1
+    )
+
+    # evaluate the network
+    logging.info(" Evaluating network...")
+    print("[INFO] evaluating network...")
+    predictions = model.predict(test_x, batch_size=32)
+    print(
+        classification_report(
+            test_y.argmax(axis=1), predictions.argmax(axis=1),
+            target_names=["fries", "beans", "potatoes"]
+        )
+    )
 
 
 if __name__ == "__main__":
