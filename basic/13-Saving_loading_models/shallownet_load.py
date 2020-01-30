0410670f0a6cbb8430e8dd01b6247f0eebf3495f
j-bd <recodata.out@gmail.com>
2020-01-30 15:54:13 +0100

    loading exemple structure

---
+++ basic/13-Saving_loading_models/shallownet_load.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 15:43:37 2020
+
+@author: j-bd
+"""
+import argparse
+
+from imutils import paths
+import numpy as np
+import cv2
+from keras.models import load_model
+
+from data_tools import ImageToArrayPreprocessor
+from data_tools import SimplePreprocessor
+from data_tools import SimpleDatasetLoader
+
+
+def main():
+    '''Launch main process'''
+
+
+if __name__ == "__main__":
+    main()
