6cf7958b8853335b1a300a3c6c2fb549c20d2239
j-bd <recodata.out@gmail.com>
2020-01-31 11:12:52 +0100

    lenet applied to mnist dataset

---
+++ basic/14-LeNet/lenet_mnist.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 11:09:02 2020
+
+@author: j-bd
+"""
+
+
+
+
+
+def main():
+    '''Launch main process'''
+
+
+
+if __name__ == "__main__":
+    main()
