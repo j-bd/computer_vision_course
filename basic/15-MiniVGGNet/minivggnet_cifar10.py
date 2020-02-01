cd3f46cf4c9e92862d1002f02435d6d264155f85
j-bd <recodata.out@gmail.com>
2020-02-01 15:11:05 +0100

    librairies import

--- basic/15-MiniVGGNet/minivggnet_cifar10.py
+++ basic/15-MiniVGGNet/minivggnet_cifar10.py
@@ -6,3 +6,22 @@ Created on Sat Feb  1 15:03:45 2020
 @author: j-bd
 """
 
+import argparse
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import classification_report
+from keras.optimizers import SGD
+from keras.datasets import cifar10
+
+from minivggnet import MiniVGGNet
+
+
+
+def main():
+    '''Launch main process'''
+
+
+if __name__ == "__main__":
+    main()
