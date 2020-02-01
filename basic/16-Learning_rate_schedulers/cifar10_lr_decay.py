da8c907ec7d1b89a22e580dcaf31e5642b34e49d
j-bd <recodata.out@gmail.com>
2020-02-01 18:41:09 +0100

    step decay definition

--- basic/16-Learning_rate_schedulers/cifar10_lr_decay.py
+++ basic/16-Learning_rate_schedulers/cifar10_lr_decay.py
@@ -19,3 +19,21 @@ from keras.datasets import cifar10
 from minivggnet import MiniVGGNet
 
 
+def step_decay(epoch):
+    '''Initialize the base initial learning rate, drop factor, and epochs to
+    drop every'''
+    init_alpha = 0.01
+    factor = 0.25
+    drop_every = 5
+
+    # Compute learning rate for the current epoch
+    alpha = init_alpha * (factor ** np.floor((1 + epoch) / drop_every))
+
+    return float(alpha)
+
+def main():
+    '''Launch main process'''
+
+
+if __name__ == "__main__":
+    main()
