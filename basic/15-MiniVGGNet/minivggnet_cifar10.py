0945ac478d35c87991dbedc35311c6aad2609be7
j-bd <recodata.out@gmail.com>
2020-02-01 15:19:29 +0100

    Arguments parser

--- basic/15-MiniVGGNet/minivggnet_cifar10.py
+++ basic/15-MiniVGGNet/minivggnet_cifar10.py
@@ -17,10 +17,33 @@ from keras.datasets import cifar10
 
 from minivggnet import MiniVGGNet
 
+#matplotlib.use("Agg")
+#plt.switch_backend('Agg')
 
+
+def arguments_parser():
+    '''Retrieve user data command'''
+    parser = argparse.ArgumentParser(
+        prog="MiniVGGNet CNNs Application",
+        usage='''%(prog)s [on Cifar10 dataset]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch custom training execution:
+        -------------------------------------
+        python3 minivggnet_cifar10.py --model path/to/folder/weights.hdf5
+        --output path/to/folder/file.png
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-m", "--model", required=True, help="path to save model"
+    )
+    parser.add_argument("-o", "--output", required=True, help="path to output")
+    args = vars(parser.parse_args())
+    return args
 
 def main():
     '''Launch main process'''
+    args = arguments_parser()
 
 
 if __name__ == "__main__":
