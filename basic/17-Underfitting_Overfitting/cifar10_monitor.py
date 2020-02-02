6ca6cb4c5b77e173bf99106bca7fdea4eb5e841c
j-bd <recodata.out@gmail.com>
2020-02-02 17:16:37 +0100

    data loader and prep plus args

--- basic/17-Underfitting_Overfitting/cifar10_monitor.py
+++ basic/17-Underfitting_Overfitting/cifar10_monitor.py
@@ -16,3 +16,65 @@ from keras.datasets import cifar10
 
 from trainingmonitor import TrainingMonitor
 from minivggnet import MiniVGGNet
+
+
+def arguments_parser():
+    '''Retrieve user data command'''
+    parser = argparse.ArgumentParser(
+        prog="Monitoring training",
+        usage='''%(prog)s [MiniVGGNet on Cifar10 dataset]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch custom training execution:
+        -------------------------------------
+        python3 cifar10_monitor.py --model path/to/folder/weights.hdf5
+        --output path/to/folder/file.png
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-m", "--model", required=True, help="path to save model"
+    )
+    parser.add_argument("-o", "--output", required=True, help="path to output")
+    args = vars(parser.parse_args())
+    return args
+
+def data_loader():
+    '''Get Cifar10 data'''
+    print("[INFO] Loading CIFAR-10 data...")
+    (train_x, train_y), (test_x, test_y) = cifar10.load_data()
+    return train_x, test_x, train_y, test_y
+
+def data_preparation(train_x, test_x, train_y, test_y):
+    '''Scaling and labelising data'''
+    train_x = train_x.astype("float") / 255.0
+    test_x = test_x.astype("float") / 255.0
+
+    # Convert the labels from integers to vectors
+    label_bin = LabelBinarizer()
+    train_y = label_bin.fit_transform(train_y)
+    test_y = label_bin.transform(test_y)
+
+    # Initialize the label names for the CIFAR-10 dataset
+    label_names = [
+        "airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse",
+        "ship", "truck"
+    ]
+
+    return train_x, test_x, train_y, test_y, label_names
+
+def main():
+    '''Launch main steps'''
+    args = arguments_parser()
+
+    # Show information on the process ID
+    print("[INFO] Process ID: {}".format(os.getpid()))
+
+    train_x, test_x, train_y, test_y = data_loader()
+
+    train_x, test_x, train_y, test_y, label_names = data_preparation(
+        train_x, test_x, train_y, test_y
+    )
+
+
+if __name__ == "__main__":
+    main()
