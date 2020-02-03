aa4af14ba43bc26e17d66ea7f97be19f2ba4c3ba
j-bd <recodata.out@gmail.com>
2020-02-03 15:51:25 +0100

    parser

--- basic/18-Ckeckpointing_models/cifar10_checkpoint_improvements.py
+++ basic/18-Ckeckpointing_models/cifar10_checkpoint_improvements.py
@@ -18,9 +18,32 @@ from keras.callbacks import ModelCheckpoint
 from minivggnet import MiniVGGNet
 
 
+def arguments_parser():
+    '''Retrieve user data command'''
+    parser = argparse.ArgumentParser(
+        prog="Create model checkpoints",
+        usage='''%(prog)s [MiniVGGNet on Cifar10 dataset]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch custom training execution:
+        -------------------------------------
+        python3 cifar10_monitor.py --weights "path/to/weights/directory"
+        --model "path/to/directory/weights.hdf5" --output "path/to/directory"
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-w", "--weights", required=True,help="path to weights directory"
+    )
+    parser.add_argument(
+        "-m", "--model", required=True, help="path to save model"
+    )
+    parser.add_argument("-o", "--output", required=True, help="path to output")
+    args = vars(parser.parse_args())
+    return args
 
 def main():
     '''Launch main steps'''
+    args = arguments_parser()
 
 
 if __name__ == "__main__":
