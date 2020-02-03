5d27cd6c29cc2c4c5a1fa74d40591a71f32af9c3
j-bd <recodata.out@gmail.com>
2020-02-03 15:44:44 +0100

    checkpoints implementation

---
+++ basic/18-Ckeckpointing_models/cifar10_checkpoint_improvements.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 15:40:02 2020
+
+@author: j-bd
+"""
+
+import os
+import argparse
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import classification_report
+from keras.optimizers import SGD
+from keras.datasets import cifar10
+from keras.callbacks import ModelCheckpoint
+
+from minivggnet import MiniVGGNet
+
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
