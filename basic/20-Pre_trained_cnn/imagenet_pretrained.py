e275a92c4569ccb21812991ae012e65a5752cf81
j-bd <recodata.out@gmail.com>
2020-02-06 10:23:58 +0100

    initialisation

---
+++ basic/20-Pre_trained_cnn/imagenet_pretrained.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 10:20:01 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+import cv2
+import numpy as np
+from keras.applications import ResNet50
+from keras.applications import InceptionV3
+from keras.applications import Xception # TensorFlow ONLY, not with Theano
+from keras.applications import VGG16
+from keras.applications import VGG19
+from keras.applications import imagenet_utils
+from keras.applications.inception_v3 import preprocess_input
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import load_img
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
