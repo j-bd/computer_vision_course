32c4deffb5174ec96ed198fbd9262b011b665d44
j-bd <recodata.out@gmail.com>
2020-02-07 18:08:15 +0100

    download process

--- basic/21-Case_study/download_images.py
+++ basic/21-Case_study/download_images.py
@@ -13,6 +13,8 @@ import os
 import requests
 
 
+URL = "https://www.e-zpassny.com/vector/jcaptcha.do"
+
 def arguments_parser():
     '''Retrieve user data command'''
     parser = argparse.ArgumentParser(
@@ -36,9 +38,38 @@ def arguments_parser():
     args = vars(parser.parse_args())
     return args
 
+def download(args):
+    '''download the captcha images available in URL'''
+    total = 0
+
+    # loop over the number of images to download
+    for i in range(0, args["num_images"]):
+        try:
+            # try to grab a new captcha image
+            request = requests.get(URL, timeout=60)
+
+            # save the image to disk
+            path = os.path.sep.join(
+                [args["output"], "{}.jpg".format(str(total).zfill(5))]
+            )
+            file = open(path, "wb")
+            file.write(request.content)
+            file.close()
+
+            # update the counter
+            print("[INFO] downloaded: {}".format(path))
+            total += 1
+
+        # handle if any exceptions are thrown during the download process
+        except:
+            print("[INFO] error downloading image...")
+        # insert a small sleep to be courteous to the server
+        time.sleep(0.1)
+
 def main():
     '''Launch main steps'''
     args = arguments_parser()
+    download(args)
 
 
 if __name__ == "__main__":
