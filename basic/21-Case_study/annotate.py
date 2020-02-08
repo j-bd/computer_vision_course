21ec29592e7fdbe3d6e880f98b626c5f30074eed
j-bd <recodata.out@gmail.com>
2020-02-08 15:56:11 +0100

    raw creation

--- basic/21-Case_study/annotate.py
+++ basic/21-Case_study/annotate.py
@@ -6,3 +6,17 @@ Created on Sat Feb  8 15:47:54 2020
 @author: j-bd
 """
 
+import argparse
+import os
+
+import imutils
+from imutils import paths
+import cv2
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
