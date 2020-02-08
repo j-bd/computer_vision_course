a805c5ea6603b2e450d9ac6b609d41bfad9cbc2a
j-bd <recodata.out@gmail.com>
2020-02-08 15:43:00 +0100

    code linter

--- basic/21-Case_study/download_images.py
+++ basic/21-Case_study/download_images.py
@@ -16,7 +16,6 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 #URL = "https://www.e-zpassny.com/vector/jcaptcha.do"
-URL = "http://allwebco-templates.com/support/script-simple-captcha.htm"
 
 def arguments_parser():
     '''Retrieve user data command'''
@@ -46,7 +45,7 @@ def download(args):
     total = 0
 
     # loop over the number of images to download
-    for i in range(0, args["num_images"]):
+    for i in range(0, args["nb_images"]):
         try:
             # try to grab a new captcha image
             request = requests.get(URL, timeout=60)
@@ -72,17 +71,17 @@ def download(args):
 def create(args):
     '''Create 'jpg' file of 4 numbers'''
     total = 0
-    for i in range(0, args["num_images"]):
-        img = Image.new('RGB', (200, 60), color = (255, 255, 255))
+    for i in range(0, args["nb_images"]):
+        img = Image.new('RGB', (200, 60), color=(255, 255, 255))
         fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
         numbers = (
             f"{random.randint(0,9)} {random.randint(0,9)} {random.randint(0,9)} "
             f"{random.randint(0,9)}"
         )
         draw = ImageDraw.Draw(img)
-        draw.text((10,10), numbers, font=fnt, fill=(0, 0, 0))
+        draw.text((10, 10), numbers, font=fnt, fill=(0, 0, 0))
         path = os.path.sep.join(
-                [args["output"], "{}.jpg".format(str(total).zfill(5))]
+            [args["output"], "{}.jpg".format(str(total).zfill(5))]
         )
         img.save(path)
         total += 1
