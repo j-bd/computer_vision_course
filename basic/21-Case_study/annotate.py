c8fb4d548af851373dbddd1c698f7c34079ffa81
j-bd <recodata.out@gmail.com>
2020-02-08 16:03:05 +0100

    args parser

--- basic/21-Case_study/annotate.py
+++ basic/21-Case_study/annotate.py
@@ -14,8 +14,33 @@ from imutils import paths
 import cv2
 
 
+def arguments_parser():
+    '''Retrieve user data command'''
+    parser = argparse.ArgumentParser(
+        prog="Case study",
+        usage='''%(prog)s [Annotate data]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch custom training execution:
+        -------------------------------------
+        python3 annotate.py
+        --input "path/to/input/directory"
+        --annot "path/to/output/annotation/directory"
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-i", "--input", required=True, help="path to input directory of images"
+    )
+    parser.add_argument(
+        "-a", "--annot", required=True,
+        help="path to output directory of annotations"
+    )
+    args = vars(parser.parse_args())
+    return args
+
 def main():
     '''Launch main steps'''
+    args = arguments_parser()
 
 
 if __name__ == "__main__":
