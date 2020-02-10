e441ff7c5c70ed3cc2809257c9a164235243500e
j-bd <recodata.out@gmail.com>
2020-02-10 17:03:30 +0100

    transpose keras to tensorflow in order to use tensorboard

--- basic/21-Case_study/lenet.py
+++ basic/21-Case_study/lenet.py
@@ -8,13 +8,13 @@ Created on Fri Jan 31 10:38:05 2020
 INPUT => CONV => ReLU => POOL => CONV => ReLU => POOL => FC => ReLU => FC
 """
 
-from keras.models import Sequential
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Flatten
-from keras.layers.core import Dense
-from keras import backend as K
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
 
 
 class LeNet:
