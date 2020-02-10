fd88398eb9b47fa3e9ed9147ad540a88db932736
j-bd <recodata.out@gmail.com>
2020-02-10 17:57:43 +0100

    Arguments parser

--- basic/21-Case_study/test_model.py
+++ basic/21-Case_study/test_model.py
@@ -19,9 +19,32 @@ from imutils import paths
 from captchahelper import preprocess
 
 
+def arguments_parser():
+    '''Retrieve user data command'''
+    parser = argparse.ArgumentParser(
+        prog="Case study",
+        usage='''%(prog)s [Testing CNNs]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch custom training execution:
+        -------------------------------------
+        python3 train_model.py
+        --input "path/to/dataset/directory" --model "path/to/output/model.hdf5"
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-i", "--input", required=True, help="path to input dataset"
+    )
+    parser.add_argument(
+        "-m", "--model", required=True, help="path to input model"
+    )
+    args = vars(parser.parse_args())
+    return args
 
 def main():
     '''Launch main steps'''
+    args = arguments_parser()
 
 
 if __name__ == "__main__":
