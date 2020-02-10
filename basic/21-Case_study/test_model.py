feccef20de969b101694130f57f2d686f2537ed3
j-bd <recodata.out@gmail.com>
2020-02-10 17:35:50 +0100

    prog init

---
+++ basic/21-Case_study/test_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 17:32:21 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+import cv2
+import numpy as np
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import imutils
+from imutils import contours
+from imutils import paths
+
+from captchahelper import preprocess
+
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
