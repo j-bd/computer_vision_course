34dab69b3847e7752a3e453a50907fec83475da8
j-bd <recodata.out@gmail.com>
2020-02-11 09:47:09 +0100

    image preprocessing and contours

--- basic/21-Case_study/test_model.py
+++ basic/21-Case_study/test_model.py
@@ -42,9 +42,47 @@ def arguments_parser():
     args = vars(parser.parse_args())
     return args
 
+def image_preprocess(path):
+    '''Greyscale, pad and threshold image'''
+    image = cv2.imread(path)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    gray = cv2.copyMakeBorder(gray, 20, 20, 20, 20, cv2.BORDER_REPLICATE)
+
+    thresh = cv2.threshold(
+        gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU
+    )[1]
+    return thresh
+
+def contours_finder(thresh):
+    '''Determine contours of elements'''
+    cnts = cv2.findContours(
+        thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+    cnts = cnts[1] if imutils.is_cv3() else cnts[0]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:4]
+    cnts = contours.sort_contours(cnts)[0]
+    # initialize the output image as a "grayscale" image with 3
+    # channels along with the output predictions
+    output = cv2.merge([gray] * 3)
+    predictions = []
+
 def main():
     '''Launch main steps'''
     args = arguments_parser()
+
+    print("[INFO] Loading pre-trained network...")
+    model = load_model(args["model"])
+
+    # randomly sample a few of the input images
+    image_paths = list(paths.list_images(args["input"]))
+    image_paths = np.random.choice(image_paths, size=(10,), replace=False)
+
+    for path in paths:
+        thresh = image_preprocess(path)
+        cnts = contours_finder(thresh)
+
 
 
 if __name__ == "__main__":
