785e17f4b8544512e04e3ee8f9fef7f08ca4d833
j-bd <recodata.out@gmail.com>
2020-02-12 10:07:53 +0100

    model training implementation

--- basic/22-Smile_detection_video/train_model.py
+++ basic/22-Smile_detection_video/train_model.py
@@ -18,6 +18,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras import utils
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import ModelCheckpoint
 from imutils import paths
 
 from lenet import LeNet
@@ -32,9 +34,9 @@ def arguments_parser():
         To lauch custom training execution:
         -------------------------------------
         python3 train_model.py
-        --dataset "path/to/dataset/directory" --model "path/to/output/model.hdf5"
-        --weights "path/to/weights/directory" --tboutput "path/to/directory"
-        --history "path/to/directory/history.png"
+        --dataset "path/to/dataset/directory" --model "path/to/model/directory"
+        --tboutput "path/to/directory" --history "path/to/directory/history.png"
+
         All arguments are mandatory.
         '''
     )
@@ -43,9 +45,6 @@ def arguments_parser():
     )
     parser.add_argument(
         "-m", "--model", required=True, help="path to save 'file.hdf5' model"
-    )
-    parser.add_argument(
-        "-w", "--weights", required=True, help="path to weights directory"
     )
     parser.add_argument(
         "-tb", "--tboutput", required=True, help="path to TensorBoard directory"
@@ -90,15 +89,55 @@ def data_preparation(dataset, labels):
     (train_x, test_x, train_y, test_y) = train_test_split(
         dataset, labels, test_size=0.20, stratify=labels, random_state=42
     )
-    return train_x, test_x, train_y, test_y, class_weight
+    return train_x, test_x, train_y, test_y, class_weight, label_enc.classes_
+
+def checkpoint_call(directory):
+    '''Return a callback checkpoint configuration to save only the best model'''
+    fname = os.path.sep.join(
+        [directory, "weights.hdf5"]
+    )
+    checkpoint = ModelCheckpoint(
+        fname, monitor="val_loss", mode="min", save_best_only=True,
+        verbose=1
+    )
+    return checkpoint
+
+def lenet_training(args, train_x, test_x, train_y, test_y, class_weight):
+    '''Launch the lenet training'''
+    print("[INFO] Compiling model...")
+    model = LeNet.build(width=28, height=28, depth=1, classes=2)
+    model.compile(
+        loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
+    )
+    model.summary()
+
+    # Callbacks creation
+    checkpoint_save = checkpoint_call(args["model"])
+    tensor_board = TensorBoard(
+        log_dir=args["tboutput"], histogram_freq=1, write_graph=True,
+        write_images=True
+    )
+    callbacks = [checkpoint_save, tensor_board]
+
+    print("[INFO] Training network...")
+    history = model.fit(
+        train_x, train_y, validation_data=(test_x, test_y),
+        class_weight=class_weight, batch_size=64, epochs=15,
+        callbacks=callbacks, verbose=1
+    )
+    return history, model
 
 def main():
     '''Launch main steps'''
     args = arguments_parser()
     dataset, labels = data_loader(args["dataset"])
 
-    train_x, test_x, train_y, test_y, class_weight = data_preparation(
+    train_x, test_x, train_y, test_y, class_weight, lab_name = data_preparation(
         dataset, labels
+    )
+
+    history, model = lenet_training(
+        args, train_x, test_x, train_y, test_y, class_weight
     )
 
 
