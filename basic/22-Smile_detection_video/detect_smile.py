6937aa1cf69e69f6293577a8e4f4cd87dc17c3e2
j-bd <recodata.out@gmail.com>
2020-02-12 11:21:29 +0100

    smile detection init

---
+++ basic/22-Smile_detection_video/detect_smile.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 11:18:31 2020
+
+@author: j-bd
+"""
+
+
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
