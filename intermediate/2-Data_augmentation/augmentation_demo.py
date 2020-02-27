cae0fded151c2f3d09f895a4e7c5f23795d0fd27
j-bd <recodata.out@gmail.com>
2020-02-27 08:59:22 +0100

    initial commit

---
+++ intermediate/2-Data_augmentation/augmentation_demo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 08:56:52 2020
+
+@author: j-bd
+"""
+
+import argparse
+
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import load_img
+
+
+
+def main():
+    '''Launch main process'''
+
+
+if __name__ == "__main__":
+    main()
