3ea3c8eaa5ea4eabefa6fe266c338a2c6ca3d469
j-bd <recodata.out@gmail.com>
2020-02-28 17:51:53 +0100

    argument parser

--- intermediate/2-Data_augmentation/minivggnet_flowers17_data_aug.py
+++ intermediate/2-Data_augmentation/minivggnet_flowers17_data_aug.py
@@ -18,16 +18,43 @@ from sklearn.metrics import classification_report
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 from data_tools import ImageToArrayPreprocessor, SimpleDatasetLoader
 from aspectawarepreprocessor import AspectAwarePreprocessor
 from minivggnet_tf import MiniVGGNet
 
 
+def arguments_parser():
+    '''Retrieve user data command'''
+    parser = argparse.ArgumentParser(
+        prog="MiniVGGNet applied to Flowers17 dataset",
+        usage='''%(prog)s [with data augmentation]''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch custom training execution:
+        -------------------------------------
+        python3 minivggnet_flowers17_data_aug.py
+        --dataset "path/to/dataset/directory" --output "path/to/model/directory"
+        --tboutput "path/to/directory"
 
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-d", "--dataset", required=True, help="path to input dataset"
+    )
+    parser.add_argument(
+        "-o", "--output", required=True, help="directory to save model and plot"
+    )
+    parser.add_argument(
+        "-tb", "--tboutput", required=True, help="path to TensorBoard directory"
+    )
+    args = vars(parser.parse_args())
+    return args
 
 def main():
     '''Launch main steps'''
+    args = arguments_parser()
 
 
 if __name__ == "__main__":
