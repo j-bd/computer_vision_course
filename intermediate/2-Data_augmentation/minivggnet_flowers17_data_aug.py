960c85d6de618f375630233d11bef213a7d62654
j-bd <recodata.out@gmail.com>
2020-02-28 17:53:47 +0100

    data loading

--- intermediate/2-Data_augmentation/minivggnet_flowers17_data_aug.py
+++ intermediate/2-Data_augmentation/minivggnet_flowers17_data_aug.py
@@ -52,9 +52,24 @@ def arguments_parser():
     args = vars(parser.parse_args())
     return args
 
+def data_loader(data_directory):
+    '''Load data and corresponding labels from disk'''
+    print("[INFO] Loading images...")
+    # Grab the list of images that we’ll be describing, then extractthe class
+    # label names from the image paths
+    image_paths = list(paths.list_images(data_directory))
+    labels = [species.split(os.path.sep)[-2] for species in image_paths]
+    cl_labels = [str(x) for x in np.unique(labels)]
+
+    return image_paths, cl_labels
+
 def main():
     '''Launch main steps'''
     args = arguments_parser()
+
+    image_paths, cl_labels = data_loader(args["dataset"])
+
+
 
 
 if __name__ == "__main__":
