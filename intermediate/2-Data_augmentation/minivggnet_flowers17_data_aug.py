8774322b47ca1b581404461c6a4a667841fdac3a
j-bd <recodata.out@gmail.com>
2020-02-29 10:16:16 +0100

    linter

--- intermediate/2-Data_augmentation/minivggnet_flowers17_data_aug.py
+++ intermediate/2-Data_augmentation/minivggnet_flowers17_data_aug.py
@@ -10,6 +10,7 @@ import os
 import argparse
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from imutils import paths
 from sklearn.preprocessing import LabelBinarizer
@@ -95,6 +96,92 @@ def data_preparation(image_paths):
 
     return train_x, test_x, train_y, test_y, img_augm_gene
 
+def checkpoint_call(directory):
+    '''Return a callback checkpoint configuration to save only the best model'''
+    fname = os.path.sep.join([directory, "weights.hdf5"])
+    checkpoint = ModelCheckpoint(
+        fname, monitor="val_loss", mode="min", save_best_only=True,
+        verbose=1
+    )
+    return checkpoint
+
+def cnn_training(args, train_x, test_x, train_y, test_y, cl_labels, img_augm_gene):
+    '''Launch the MiniVGGNet training'''
+    print("[INFO] Compiling model...")
+    opt = SGD(lr=0.05)
+    model = MiniVGGNet.build(
+        width=64, height=64, depth=3, classes=len(cl_labels)
+    )
+    model.compile(
+        loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"]
+    )
+    model.summary()
+
+    # Callbacks creation
+    checkpoint_save = checkpoint_call(args["output"])
+    tensor_board = TensorBoard(
+        log_dir=args["tboutput"], histogram_freq=1, write_graph=True,
+        write_images=True
+    )
+    callbacks = [checkpoint_save, tensor_board]
+
+    print("[INFO] Training network...")
+    history = model.fit_generator(
+        img_augm_gene.flow(train_x, train_y, batch_size=32),
+        validation_data=(test_x, test_y), steps_per_epoch=len(train_x) // 32,
+        epochs=100, callbacks=callbacks, verbose=1
+    )
+    return history, model
+
+def model_evaluation(model, test_x, test_y, label_names, directory):
+    '''Display on terminal command the quality of model's predictions'''
+    print("[INFO] Evaluating network...")
+    predictions = model.predict(test_x, batch_size=64)
+
+    report = classification_report(
+        test_y.argmax(axis=1), predictions.argmax(axis=1),
+        target_names=label_names, output_dict=True
+    )
+    dataframe = pd.DataFrame.from_dict(report, orient="index", )
+    dataframe.to_csv(
+        os.path.sep.join([directory, "classification_report.csv"]), index=True
+    )
+
+    print(
+        classification_report(
+            test_y.argmax(axis=1), predictions.argmax(axis=1),
+            target_names=label_names
+        )
+    )
+
+def display_learning_evol(history_dic, directory):
+    '''Plot the training loss and accuracy'''
+    fname = os.path.sep.join([directory, "loss_accuracy_history.png"])
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(
+        np.arange(0, len(history_dic.history["loss"])),
+        history_dic.history["loss"], label="train_loss"
+    )
+    plt.plot(
+        np.arange(0, len(history_dic.history["val_loss"])),
+        history_dic.history["val_loss"], label="val_loss"
+    )
+    plt.plot(
+        np.arange(0, len(history_dic.history["accuracy"])),
+        history_dic.history["accuracy"], label="train_acc"
+    )
+    plt.plot(
+        np.arange(0, len(history_dic.history["val_accuracy"])),
+        history_dic.history["val_accuracy"], label="val_accuracy"
+    )
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend()
+    plt.savefig(fname)
+
+
 def main():
     '''Launch main steps'''
     args = arguments_parser()
@@ -104,6 +191,14 @@ def main():
     train_x, test_x, train_y, test_y, img_augm_gene = data_preparation(
         image_paths
     )
+
+    history, model = cnn_training(
+        args, train_x, test_x, train_y, test_y, cl_labels, img_augm_gene
+    )
+
+    model_evaluation(model, test_x, test_y, cl_labels, args["output"])
+
+    display_learning_evol(history, args["output"])
 
 
 if __name__ == "__main__":
