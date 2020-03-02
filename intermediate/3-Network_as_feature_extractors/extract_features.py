78d45ed3e62031fdd092b2c607ce34a4f78a7286
j-bd <recodata.out@gmail.com>
2020-03-02 17:56:08 +0100

    data loader

--- intermediate/3-Network_as_feature_extractors/extract_features.py
+++ intermediate/3-Network_as_feature_extractors/extract_features.py
@@ -10,7 +10,6 @@ import os
 import argparse
 import random
 
-from imutils import paths
 import numpy as np
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.applications import imagenet_utils
@@ -20,21 +19,23 @@ from sklearn.preprocessing import LabelEncoder
 import progressbar
 
 from hdf5datasetwriter import HDF5DatasetWriter
+from imutils import paths
 
 
 def arguments_parser():
     '''Retrieve user data command'''
     parser = argparse.ArgumentParser(
-        prog="",
-        usage='''%(prog)s []''',
+        prog="Feature Extraction Process",
+        usage='''%(prog)s [VGG16]''',
         formatter_class=argparse.RawDescriptionHelpFormatter, description='''
         To lauch custom training execution:
         -------------------------------------
         python3 .py
         --dataset "path/to/dataset/directory" --output "path/to/model/directory"
         --tboutput "path/to/directory"
+        --batch-size 32 --buffer-size 1000
 
-        All arguments are mandatory.
+        The three first arguments are mandatory.
         '''
     )
     parser.add_argument(
@@ -57,9 +58,26 @@ def arguments_parser():
     args = vars(parser.parse_args())
     return args
 
+def data_loader(data_directory):
+    '''Load data and corresponding labels from disk'''
+    # Grab the list of images that we’ll be describing then randomly shuffle
+    # them to allow for easy training and testing splits via array slicing
+    # during training time
+    print("[INFO] Loading images...")
+    image_paths = list(paths.list_images(data_directory))
+    random.shuffle(image_paths)
+
+    # Extract the class labels from the image paths then encode the labels
+    labels = [path.split(os.path.sep)[-2] for path in image_paths]
+    lab_enc = LabelEncoder()
+    labels = lab_enc.fit_transform(labels)
+
+    return image_paths, labels
+
 def main():
     '''Launch main steps'''
     args = arguments_parser()
+    image_paths, labels = data_loader(args["dataset"])
 
 
 if __name__ == "__main__":
