cb185a21384d40d8698ba39695e12cfaf476c6f8
j-bd <recodata.out@gmail.com>
2020-03-02 17:28:18 +0100

    initial commit

---
+++ intermediate/3-Network_as_feature_extractors/extract_features.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 17:20:54 2020
+
+@author: j-bd
+"""
+
+import os
+import argparse
+import random
+
+from imutils import paths
+import numpy as np
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications import imagenet_utils
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from sklearn.preprocessing import LabelEncoder
+import progressbar
+
+from hdf5datasetwriter import HDF5DatasetWriter
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
