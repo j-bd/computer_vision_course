7aed2540616fb96b1ddfd924e6ba81db8ac93a80
j-bd <recodata.out@gmail.com>
2020-03-02 17:31:06 +0100

    arguments parser

--- intermediate/3-Network_as_feature_extractors/extract_features.py
+++ intermediate/3-Network_as_feature_extractors/extract_features.py
@@ -22,8 +22,44 @@ import progressbar
 from hdf5datasetwriter import HDF5DatasetWriter
 
 
+def arguments_parser():
+    '''Retrieve user data command'''
+    parser = argparse.ArgumentParser(
+        prog="",
+        usage='''%(prog)s []''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch custom training execution:
+        -------------------------------------
+        python3 .py
+        --dataset "path/to/dataset/directory" --output "path/to/model/directory"
+        --tboutput "path/to/directory"
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-d", "--dataset", required=True, help="path to input dataset"
+    )
+    parser.add_argument(
+        "-o", "--output", required=True, help="directory to save model and plot"
+    )
+    parser.add_argument(
+        "-b", "--batch-size", type=int, default=32,
+        help="batch size of images to be passed through network"
+    )
+    parser.add_argument(
+        "-s", "--buffer-size", type=int, default=1000,
+        help="size of feature extraction buffer"
+    )
+    parser.add_argument(
+        "-tb", "--tboutput", required=True, help="path to TensorBoard directory"
+    )
+    args = vars(parser.parse_args())
+    return args
+
 def main():
     '''Launch main steps'''
+    args = arguments_parser()
 
 
 if __name__ == "__main__":
