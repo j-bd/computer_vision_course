047e5898c377d90bd8ad969d85a341846c1b65d8
j-bd <recodata.out@gmail.com>
2020-03-06 17:36:36 +0100

    arguments parser

--- intermediate/3-Network_as_feature_extractors/train_model.py
+++ intermediate/3-Network_as_feature_extractors/train_model.py
@@ -16,8 +16,40 @@ from sklearn.metrics import classification_report
 
 
 
+def arguments_parser():
+    '''Retrieve user data command'''
+    parser = argparse.ArgumentParser(
+        prog="",
+        usage='''%(prog)s []''',
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+        To lauch custom training execution:
+        -------------------------------------
+        python3 .py
+        --dataset "path/to/dataset/directory" --output "path/to/model/directory"
+        --tboutput "path/to/directory"
+
+        All arguments are mandatory.
+        '''
+    )
+    parser.add_argument(
+        "-db", "--database", required=True, help="path HDF5 database"
+    )
+    parser.add_argument(
+        "-m", "--model", required=True, help="path to output model"
+    )
+    parser.add_argument(
+        "-j", "--jobs", type=int, default=-1,
+        help="numbers of jobs to run when tuning hyperparameters"
+    )
+    parser.add_argument(
+        "-tb", "--tboutput", required=True, help="path to TensorBoard directory"
+    )
+    args = vars(parser.parse_args())
+    return args
+
 def main():
     '''Launch main steps'''
+    args = arguments_parser()
 
 
 if __name__ == "__main__":
