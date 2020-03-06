812d021fc69a40858da27b17a6008618d27da6e7
j-bd <recodata.out@gmail.com>
2020-03-06 17:23:44 +0100

    initial commit

---
+++ intermediate/3-Network_as_feature_extractors/train_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 17:20:48 2020
+
+@author: j-bd
+"""
+
+import argparse
+import pickle
+
+import h5py
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+
+
+
+def main():
+    '''Launch main steps'''
+
+
+if __name__ == "__main__":
+    main()
